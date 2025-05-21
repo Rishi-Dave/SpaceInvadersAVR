cff79293e27dfbc60f53aafed4eb2103404f492b
Rishi-Dave <rishipdave@gmail.com>
2025-05-21 13:32:26 -0700

    initial files

--- README.md
+++
@@ -1,2 +0,0 @@
-# SpaceInvadersAVR
-For my Embedded Systems class, I created a Space Invaders game using an ST7735 LCD Graphic Display. The required constraint was to only use the AVR library and not the arduino library, meaning that I needed a high level bit-by-bit understanding of the LCD display, along with my other components, in able to create the game.
---
+++ src/ST7737.h
@@ -0,0 +1,54 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "spiAVR.h"
+#include "helper.h"
+
+#ifndef ST7735_H
+#define St7735_H
+
+#define RST_PIN  0 // Assuming PB0
+#define A0_PIN   1 // Assuming PB1
+#define CS_PIN   2 // Assuming PB2
+
+void Send_Command(uint8_t cmd) {
+    SetPin(&PORTB, A0_PIN, 0); // DC low (command mode)
+    SetPin(&PORTB, CS_PIN, 0); // CS low (select device)
+    SPI_SEND(cmd);
+    SetPin(&PORTB, CS_PIN, 1);
+}
+
+void Send_Data(uint8_t data) {
+    SetPin(&PORTB, A0_PIN, 1); // DC low (command mode)
+    SetPin(&PORTB, CS_PIN, 0); // CS low (select device)
+    SPI_SEND(data);
+    SetPin(&PORTB, CS_PIN, 1);
+}
+
+void HardwareReset(){
+    SetPin(&PORTB, RST_PIN, 0);
+    _delay_ms(200);
+    SetPin(&PORTB, RST_PIN, 1);;
+    _delay_ms(200);
+}
+void ST7735_init(){
+    HardwareReset();
+ 
+    Send_Command(0x01); // SWRESET (Software Reset)
+    _delay_ms(150);
+
+    Send_Command(0x11); // SLPOUT (Sleep Out)
+    _delay_ms(200);
+
+    Send_Command(0x3A); // COLMOD (Color Mode)
+    Send_Data(0x05); //for 18 bit color mode. You can pick any color mode you want
+    _delay_ms(10);
+
+    Send_Command(0x29); // DISPON (Display On)
+    _delay_ms(200);     
+
+    _delay_ms(10);
+}
+
+
+#endif
