91443d67486c27c12ed942e3a17dbe3810fd841b
Rishi-Dave <rishipdave@gmail.com>
2025-05-29 12:43:35 -0700

    alien shooting complete

--- src/headers/helper.h
+++ src/headers/helper.h
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-
+#include "Objects.h"
 #ifndef HELPER_H
 #define HELPER_H
 unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
@@ -21,7 +21,40 @@ void SetPin(volatile uint8_t *port, uint8_t pin, uint8_t state) {
     }
 }
 
+int direction(int x, int y){
+  if(x < 500 ){
+    return 4;
+  }
+  else if(x > 600 ) {
+    return 3;
+  }
+
+  else if(y < 500 ){
+    return 1;
+  }
+  else if(y > 600) {
+    return 2;
+  }
+  else{
+    return 0;
+
+  }
+
+}
  
+void ShiftLeft(Bullet** pArr, int index, int length)
+{
+  for (int i = index; i < length; i++)
+  {
+    Bullet* tmp = pArr[i - 1]; // Preserves the previous value
+    pArr[i - 1] = pArr[i]; // Overwrites the previous position with the current value
+    pArr[i] = tmp;         // Stores the previous value in the current position
+                           // All in all the first value is swapped down the array until it is at the length - 1 position                        // and all the other values are swapped to the left.
+  }
+}
+
+
+
 int nums[16] = {0b1111110, 0b0110000, 0b1101101, 0b1111001, 0b0110011, 0b1011011, 0b1011111, 0b1110000, 0b1111111, 0b1111011, 0b1110111, 0b0011111, 0b1001110, 0b0111101, 0b1001111, 0b1000111 }; 
 // a  b  c  d  e  f  g
 
